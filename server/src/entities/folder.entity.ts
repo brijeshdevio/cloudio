b9c51b659faef7895055e46bcf1bacd18bc54426
Brijesh <softbrijeshio@gmail.com>
2025-12-13 14:05:09 +0530

    feat(web): add folder path property

--- server/src/entities/folder.entity.ts
+++ server/src/entities/folder.entity.ts
@@ -11,11 +11,28 @@ export class Folder {
   @Prop({ type: Types.ObjectId, required: true, ref: 'User' })
   owner: Types.ObjectId;
 
-  @Prop({ type: Types.ObjectId, ref: 'Folder' })
-  parent: Types.ObjectId;
+  @Prop({ type: Types.ObjectId, ref: 'Folder', default: null })
+  parent: Types.ObjectId | null;
 
-  @Prop({ type: [Types.ObjectId], ref: 'Folder' })
+  @Prop({ type: [Types.ObjectId], ref: 'Folder', default: [] })
   path: Types.ObjectId[];
 }
 
 export const FolderSchema = SchemaFactory.createForClass(Folder);
+
+FolderSchema.pre('save', async function (this: FolderDocument) {
+  if (!this.parent) {
+    this.path = [];
+    return; // async hooks return instead of next()
+  }
+
+  const parentFolder = (await this.model('Folder').findById(
+    this.parent,
+  )) as FolderDocument;
+
+  if (parentFolder) {
+    this.path = [...parentFolder.path, parentFolder._id];
+  } else {
+    this.path = [];
+  }
+});
